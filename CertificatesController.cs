ab738dc4900de84997c1490bdc274a60c2a37b78
GurgenOhanyan <78640070+GurgenOhanyan@users.noreply.github.com>
2021-11-01 18:14:43 +0400

    Add files via upload

---
+++ CertificatesController.cs
@@ -0,0 +1,34 @@
+﻿using DigiSign.Manager.Areas.Api.ApiModels;
+using DigiSign.Manager.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiSign.Manager.Areas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Area("Api")]
+    [ApiController]
+    public class CertificatesController : ControllerBase
+    {
+        protected readonly IRepository _repository;
+        public CertificatesController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public GetCertificateResponse Get(int id)
+        {
+            var model = _repository.Read(1);
+            var response = new GetCertificateResponse();
+            if (model == null)
+                return response;
+            response.File = model.Body;
+            response.Password = model.Password;
+            return response;
+        }
+    }
+}
---
+++ GetCertificateResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiSign.Manager.Areas.Api.ApiModels
+{
+    public class GetCertificateResponse
+    {
+        public byte[] File { get; set; }
+        public string Password { get; set; }
+    }
+}
